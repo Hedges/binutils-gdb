951f7919644842332450da57da4fff29635b7a4e
Hedges <hedges@resync.pl>
2022-02-17 23:18:14 +0000

    Updated build scripts.

--- build_ios.sh
+++ build_ios.sh
@@ -1,5 +1,19 @@
-#rm -f -r build_ios
-mkdir build_ios
-cd build_ios
-../configure CFLAGS=-O3 --prefix=/mingw --host=x86_64-w64-mingw32 --build=x86_64-w64-mingw32 --target=aarch64-apple-darwin --with-expat --disable-binutils --disable-ld --disable-gold --disable-gas --disable-gprof --disable-sim --disable-nls --disable-multilib
+#!/bin/sh
+TRG="build_ios"
+if [ "$1" ==  "reset" ]
+then
+	if [ -d "$TRG" ] 
+	then
+		rm -f -r "$TRG"
+	fi
+	mkdir "$TRG"
+	cd "$TRG"
+	../configure CFLAGS=-O3 --prefix=/mingw --host=x86_64-w64-mingw32 --build=x86_64-w64-mingw32 --target=aarch64-apple-darwin --with-expat --disable-binutils --disable-ld --disable-gold --disable-gas --disable-gprof --disable-sim --disable-nls --disable-multilib
+else
+	if [ ! -d "$TRG" ] 
+	then
+		mkdir "$TRG"
+	fi
+	cd "$TRG"
+fi
 make -j
--- build_nx.sh
+++ build_nx.sh
@@ -1,5 +1,19 @@
-#rm -f -r build_nx
-mkdir build_nx
-cd build_nx
-../configure CFLAGS=-O3 --prefix=/mingw --host=x86_64-w64-mingw32 --build=x86_64-w64-mingw32 --target=aarch64-none-elf --with-expat --disable-binutils --disable-ld --disable-gold --disable-gas --disable-gprof --disable-sim --disable-nls --disable-multilib
+#!/bin/sh
+TRG="build_nx"
+if [ "$1" ==  "reset" ]
+then
+	if [ -d "$TRG" ] 
+	then
+		rm -f -r "$TRG"
+	fi
+	mkdir "$TRG"
+	cd "$TRG"
+	../configure CFLAGS=-O3 --prefix=/mingw --host=x86_64-w64-mingw32 --build=x86_64-w64-mingw32 --target=aarch64-apple-darwin --with-expat --disable-binutils --disable-ld --disable-gold --disable-gas --disable-gprof --disable-sim --disable-nls --disable-multilib
+else
+	if [ ! -d "$TRG" ] 
+	then
+		mkdir "$TRG"
+	fi
+	cd "$TRG"
+fi
 make -j
